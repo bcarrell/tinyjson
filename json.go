82708604e6ec7c7b4944e891320ebb2f79d2a84a
Brandon Carrell <brandoncarrell@gmail.com>
2013-12-21 22:45:20 -0500

    [add] adding json write function

--- json.go
+++ json.go
@@ -1,9 +1,17 @@
 package tinyjson
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func Write(w http.ResponseWriter, v interface{}) {
-
+	payload, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(payload)
 }
 
 func Read(r *http.Request, v interface{}) error {
